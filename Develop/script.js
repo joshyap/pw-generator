12b694e41195e0be0a8da4441945d33d43496ff8
Joshua Yap <joshuayap@Joshuas-MacBook-Pro.local>
2021-11-27 18:13:56 -0600

    working mvp

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,15 @@
 // Assignment code here
-let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let numbers = ['0','1','2','3','4','5','6','7','8','9'];
-let special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
-let pw;
+let options = [        
+  ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'], // 0
+  ['0','1','2','3','4','5','6','7','8','9'], // 1
+  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], // 2
+  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] // 3
+];
+let masterOptions = [];
 
-
-function generatePassword() {
-  validatePassword();
-
-}
-
-function validatePassword() {
+function generatePassword() {  
   let pwLength = parseInt(prompt('Choose a password length between 8 and 128 characters'));
-  let specialFlag = confirm('Click OK to use special characters.');
-  let numericFlag = confirm('Click OK to use numeric characters.');  
-  let lowerFlag = confirm('Click OK to use lower case characters.');
-  let upperFlag = confirm('Click OK to use upper case characters.');  
+  let pw = '';
 
   if (Number.isNaN(pwLength)) {
     alert('Password length must be a number');
@@ -26,14 +20,32 @@ function validatePassword() {
   } else if (pwLength > 128) {
     alert('Password length must be less than 129 characters.');  
     return null;
-  }  
+  } 
+
+  let specialFlag = confirm('Click OK to use special characters.');
+  let numericFlag = confirm('Click OK to use numeric characters.');  
+  let lowerFlag = confirm('Click OK to use lower case characters.');
+  let upperFlag = confirm('Click OK to use upper case characters.');
 
   if (specialFlag === false && numericFlag === false && lowerFlag === false && upperFlag === false) {
-    alert('You must select at least one passwor configuration option');
+    alert('You must select at least one password configuration option');
     return null;
   }  
-}
 
+  // masterOptions contains the available chars to be used in a generated pw
+  if (specialFlag) {masterOptions = masterOptions.concat(options[0])};
+  if (numericFlag) {masterOptions = masterOptions.concat(options[1])}; 
+  if (lowerFlag) {masterOptions = masterOptions.concat(options[2])};
+  if (upperFlag) {masterOptions = masterOptions.concat(options[3])};
+  // console.log(masterOptions);
+  
+  for (var i=0; i<pwLength; i++) {
+    pw += masterOptions[Math.floor(Math.random() * masterOptions.length)];
+  }
+
+  console.log(pw);
+  return pw;
+}
 
 
 // original code below
